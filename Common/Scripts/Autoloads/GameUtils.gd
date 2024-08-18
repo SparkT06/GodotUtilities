1e0199729dc83fa733d3f795c8b8d94283b3d4c5
SparkT06 <118836956+SparkT06@users.noreply.github.com>
2024-08-18 12:30:05 +0530

    Update GameUtils.gd

--- Common/Scripts/Autoloads/GameUtils.gd
+++ Common/Scripts/Autoloads/GameUtils.gd
@@ -9,12 +9,9 @@ func get_player():
 func get_enemies() -> Array:
 	return get_tree().get_nodes_in_group("Enemy");
 	
-func get_node_layer(node_name: String) -> Node:
-	var nodes_in_group = get_tree().get_nodes_in_group("Layers")
-	for node in nodes_in_group:
-		if node.name == node_name:
-			return node
-	return null
+func get_node_layer(layer_name: String) -> Node:
+	var nodes_in_group = get_tree().get_first_node_in_group(layer_name)
+	return nodes_in_group
 
 func remove_parent():
 	if self.owner == null: self.owner.queue_free();
